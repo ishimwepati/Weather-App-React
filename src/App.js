950460d2710978daeac1524dd259673461465f80
ishimwepati <wazacode@gmail.com>
2024-06-19 14:14:51 +0300

    Country Codes List added

--- src/App.js
+++ src/App.js
@@ -1,2 +1,17 @@
 import React, { useState } from 'react';
 import './App.css';
+
+const countryCodes = {
+  "US": "United States",
+  "GB": "United Kingdom",
+  "CA": "Canada",
+  "FR": "France",
+  "DE": "Germany",
+  "IN": "India",
+  "JP": "Japan",
+  "CN": "China",
+  "BR": "Brazil",
+  "ZA": "South Africa",
+  "UG": "Uganda",
+  // I'll add more Here 
+};
